3693aaa0bb2eff831861cdeac01574d38dd64017
Jeff Deville <jeffdeville@gmail.com>
2023-10-17 16:34:21 -0400

    now add single quotes around the whole thing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 secrets=$(op item list --vault $1 --format=json | op item get - --format=json | jq -r '"\(.title)=\"\(.fields[0].value)\""' | awk -v d=" " '{s=(NR==1?s:s d)$0}END{print s}')
-echo "fly_secrets=$secrets" >> $GITHUB_OUTPUT
+echo "fly_secrets='$secrets'" >> $GITHUB_OUTPUT
