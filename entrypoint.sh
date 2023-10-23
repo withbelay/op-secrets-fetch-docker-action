4b8461b354e71f6bdafef8655dfdb9835a7b39e8
Jeff Deville <jeffdeville@gmail.com>
2023-10-23 14:52:51 -0400

    hide secrets from display

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ op item list --vault $1 --format=json \
   | op item get - --format=json \
   | jq -r '"\(.title)=\"\(.fields[0].value)\""' \
   | tr '\n' '\0' \
-  | xargs -0 -t fly secrets set -a $2 --stage
+  | xargs -0 fly secrets set -a $2 --stage
