5f637806e025c81694d150b5c89904077baac6e2
Jeff Deville <jeffdeville@gmail.com>
2023-10-23 15:41:06 -0400

    use fly secrets import instead of set

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,4 @@
 op item list --vault $1 --format=json \
   | op item get - --format=json \
   | jq -r '"\(.title)=\"\(.fields[0].value)\""' \
-  | tr '\n' '\0' \
-  | xargs -0 fly secrets set -a $2 --stage
+  | fly secrets import -a $2 --stage
